102e4af6b251d37fa7b3f82cea8da38b4ebc9b87
sclark003 <60785645+sclark003@users.noreply.github.com>
2024-12-05 15:33:41 +0000

    Update StringExt.kt

--- src/main/kotlin/extensions/StringExt.kt
+++ src/main/kotlin/extensions/StringExt.kt
@@ -4,9 +4,9 @@ fun String.parseLines(): List<String> = this.split("\n")
 
 fun String.parseWhiteSpace(): List<String> = this.split("\\s+".toRegex())
 
-fun String.parseRows(): List<List<String>> {
+fun String.parseRows(): List<String> {
     val rowsList = this.parseLines()
-    val rows = rowsList.map { it.parseWhiteSpace() }
+    val rows = rowsList.map { it.parseWhiteSpace().joinToString("") }
     return rows
 }
 
@@ -19,17 +19,17 @@ fun String.parseRowsAsInts(): List<List<Int>> {
     return rows
 }
 
-fun String.parseColumns(noCols: Int = 2): List<List<String>> {
+fun String.parseColumns(noCols: Int = 2): List<String> {
     val rowLists = this.parseLines()
-    val rows = rowLists.map { it.parseWhiteSpace() }
-    val columnsList = mutableListOf<List<String>>()
+    val rows = rowLists.map { it.parseWhiteSpace().joinToString("") }
+    val columnsList = mutableListOf<String>()
     var i = 0
     while (i < noCols) {
         val col = mutableListOf<String>()
         rows.forEach { row ->
-            col.add(row[i])
+            col.add(row[i].toString())
         }
-        columnsList.add(col)
+        columnsList.add(col.joinToString(""))
         i++
     }
     return columnsList.toList()
@@ -49,4 +49,67 @@ fun String.parseColumnsAsInts(noCols: Int = 2): List<List<Int>> {
         i++
     }
     return columnsList.toList()
+}
+
+fun String.parseDiagonalDownRight(noRows: Int): List<Pair<String, List<Pair<Int, Int>>>> {
+    val rows = this.parseRows()
+    val diagonalsDownRightList = mutableListOf<Pair<String, List<Pair<Int, Int>>>>()
+    val list = mutableListOf<String>()
+    for (i in 0..((noRows*2)-2)) {
+        val listXY = mutableListOf<Pair<Int, Int>>()
+        if (i>=noRows) {
+            val xStart = i-noRows+1
+            for (y in xStart.. (noRows-1)) {
+                val x = noRows+xStart-y-1
+                list.add(rows[x][y].toString())
+                listXY.add(y to x)
+            }
+        } else {
+            for (y in 0..i) {
+                val x = i-y
+                list.add(rows[x][y].toString())
+                listXY.add(y to x)
+            }
+        }
+        diagonalsDownRightList.add(list.joinToString("") to listXY)
+        list.clear()
+    }
+    return diagonalsDownRightList
+}
+
+fun String.parseDiagonalUpLeft(noRows: Int): List<Pair<String, List<Pair<Int, Int>>>> {
+    val rows = this.parseRows()
+    val diagonalsInt = mutableListOf<List<Int>>()
+    val list = mutableListOf<Int>()
+    for (i in 0..(noRows*2)-2) {
+        for (x in i downTo 0) {
+            list.add(x)
+        }
+        if (i>=noRows){
+            val reverseList = list.reversed()
+            val j = i-noRows+1
+            val subList = reverseList.subList(j, reverseList.size-j)
+            diagonalsInt.add(subList)
+        } else {
+            diagonalsInt.add(list.reversed())
+        }
+        list.clear()
+    }
+
+    val diagonalsList = mutableListOf<Pair<String, List<Pair<Int, Int>>>>()
+    for (i in 0..diagonalsInt.size-1) {
+        val listXY = mutableListOf<Pair<Int, Int>>()
+        val yList = diagonalsInt[i]
+        val xList = diagonalsInt[diagonalsInt.size-1-i].reversed()
+        val diagonolString = mutableListOf<String>()
+        for (j in 0..xList.size-1) {
+            val y = yList[j]
+            val x = xList[yList.size-1-j]
+            diagonolString.add(rows[y][x].toString())
+            listXY.add(x to y)
+        }
+        diagonalsList.add(diagonolString.joinToString("") to listXY)
+        diagonolString.clear()
+    }
+    return diagonalsList
 }
